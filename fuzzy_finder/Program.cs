34b7c35fe1fbdee7a8c6272074453592254a3dc0
YabseraBogale <yabserapython@gmail.com>
2026-05-05 19:44:46 +0300

    making fuzzy finder

--- fuzzy_finder/Program.cs
+++ fuzzy_finder/Program.cs
@@ -1,33 +1,36 @@
-﻿string Soundex(string str)
+﻿string? Soundex(string str)
 {
-     str=str.ToLower();
-   
-    for(int i = 1; i < str.Length; i++)
+    str=str.ToLower();
+    
+    Stack<char> letters=new Stack<char>();
+    letters.Push(str[0]);
+    for(int i = 1; i < str.Length && letters.Capacity<4; i++)
     {
         if(str[i]=='b' || str[i]=='f' || str[i]=='p' || str[i] == 'v')
         {
-            str=str.Replace(str[i],'1');
+            letters.Push('1');
         } else if (str[i]=='c' || str[i]=='g' || str[i]=='j' || str[i] == 'k' || str[i]=='q' || str[i] == 's' || str[i]=='x' || str[i] == 'z')
         {
-            str=str.Replace(str[i],'2');
+            letters.Push('2');
         } else if (str[i]=='d' || str[i]=='t')
         {
-            str=str.Replace(str[i],'3');
+            letters.Push('3');
         } else if (str[i]=='l')
         {
-            str=str.Replace(str[i],'4');
+            letters.Push('4');
         }else if (str[i]=='m' || str[i]=='n')
         {
-            str=str.Replace(str[i],'5');
+            letters.Push('5');
         }else if (str[i]=='r')
         {
-            str=str.Replace(str[i],'6');
+            letters.Push('6');
         }else if (str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i] == 'o' || str[i]=='u' || str[i] == 'h' || str[i]=='w' || str[i] == 'y')
         {
-            str=str.Replace(str[i],'0');
+            letters.Push('0');
         }
     }
-    return str;
+    
+    return letters.ToArray().Reverse().ToArray().ToString();
     
 }
 
